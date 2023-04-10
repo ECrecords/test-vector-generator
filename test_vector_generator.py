28fe84c588e984ebdafac1151c2895c43a4bb045
Elvis Chino-Islas <echinoislas+github@gmail.com>
2023-04-10 15:42:48 -0700

    changed json format, encoding is now part of operation class

--- test_vector_generator.py
+++ test_vector_generator.py
@@ -2,23 +2,25 @@ import numpy as np
 import json
 import argparse
 
+
 def main():
     NUM_BITS: int = 32
     OPCODE_BITS: int = 4
 
-    parser = argparse.ArgumentParser(description='Generate test vectors from a JSON file.')
-    parser.add_argument('-j', '--json', type=str, help='The name of the input JSON file.')
-    parser.add_argument('-f', '--filename', type=str, help='The name of the output text file.', default='test_vectors.txt', nargs='?')
+    parser = argparse.ArgumentParser(
+        description='Generate test vectors from a JSON file.')
+    parser.add_argument('-j', '--json', type=str,
+                        help='The name of the input JSON file.')
+    parser.add_argument('-f', '--filename', type=str,
+                        help='The name of the output text file.', default='test_vectors.txt', nargs='?')
     args = parser.parse_args()
 
     if not args.json:
-        parser.error('No input file specified. Please provide an input file name.')
+        parser.error(
+            'No input file specified. Please provide an input file name.')
 
     with open(args.json, 'r') as input_file:
         json_data: dict = json.load(input_file)
-        encoding: dict = json_data['encoding']
-        test_vectors: dict = json_data['test_vectors']
-
         with open(args.filename, 'w') as output_file:
 
             code_label = [
@@ -33,7 +35,8 @@ def main():
             np.savetxt(output_file, labels, fmt='%s', newline='\t\t')
             output_file.write('\n')
 
-            for operation, data in test_vectors.items():
+            for operation, data in json_data.items():
+                opcode: str = np.array([bit for bit in data['inputs']['encoding']]).astype(str)
                 a_vectors: dict = data['inputs']['a_vectors']
                 b_vectors: dict = data['inputs']['b_vectors']
                 c_outputs: dict = data['outputs']['c_output']
@@ -42,6 +45,7 @@ def main():
                 c_flags: dict = data['outputs']['c_flag']
                 v_flags: dict = data['outputs']['v_flag']
 
+                print(f'{opcode}:\n{a_vectors}\n{b_vectors}\n{c_outputs}')
                 if not (a_vectors or b_vectors or c_outputs or z_flags or n_flags or c_flags or v_flags):
                     continue
                 else:
@@ -58,8 +62,6 @@ def main():
                         continue
 
                 for a, b, c, zf, nf, cf, vf in zip(a_vectors, b_vectors, c_outputs, z_flags, n_flags, c_flags, v_flags):
-                    opcode: str = np.array(
-                        [bit for bit in encoding[operation]]).astype(str)
                     a_bin: np.array = np.array(
                         [bit for bit in np.binary_repr(a, NUM_BITS)]).astype(str)
                     b_bin: np.array = np.array(
